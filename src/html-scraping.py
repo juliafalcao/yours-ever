b3b6996dde875da7420dfa4701ed25633168e392
juliafalcao <juliafsfalcao@hotmail.com>
2019-03-30 16:23:50 -0300

    na guerra contra o html, a batalha de hoje eu venci

--- src/html-scraping.py
+++ src/html-scraping.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup as bs
 from bs4 import NavigableString
 import pandas as pd
 
+newline = "\n"
 
 export_path = "data\\vw\\letters\\output"
 letters_path = "data\\vw\\letters"
@@ -10,85 +11,85 @@ letters_path = "data\\vw\\letters"
 vw = pd.DataFrame({"id": [], "date": [], "receiver": [], "place": [], "text": []})
 
 for letters_file in os.listdir(letters_path): # loop through files in dir
-    if ".htm" not in letters_file:
-        continue # skip subdirectories
+# for letters_file in ["35Lettera1.htm"]:
+	if ".htm" not in letters_file:
+		continue # skip subdirectories
 
-    print(f"Reading '{letters_file}'...")
+	print(f"Reading '{letters_file}'...")
 
-    with codecs.open(f"{letters_path}\\{letters_file}", mode="r", encoding="utf-8") as file:
-        file_content = file.read()
+	with codecs.open(f"{letters_path}\\{letters_file}", mode="r", encoding="utf-8") as file:
+		file_content = file.read()
 
-    soup = bs(file_content, "html.parser")
-    html = soup.find("html")
-    body = html.find("body")
-    soup = body
+	soup = bs(file_content, "html.parser")
+	html = soup.find("html")
+	body = html.find("body")
+	soup = body
 
-    # headers separated in ids and receivers
-    headers = []
-    for h3item in soup.find_all("h3"):
-        if h3item.has_attr("id") or h3item.has_attr("title"): # only get actual letter headers
-            headers.append(h3item.get_text())
+	# start reading letters
+	delimiter = "link"
+	to_ignore = ["date", "card", "sigil_not_in_toc", "imgbrd", "margl"]
+	count_per_file = 0
 
-    ids = [header[:header.find(":")] for header in headers]
-    receivers = [header[header.find(":")+1:].lstrip() for header in headers]
-    print(f"Found {len(headers)} headers ({len(ids)} IDs and {len(receivers)} receivers).")
+	for h3item in soup.find_all("h3"): # start from h3 header
+		if isinstance(h3item, NavigableString):
+			continue
 
-    # dates
-    dates = [item.get_text().replace("[", "").replace("]", "") for item in soup.find_all("p", {"class": "time"})]
-    print(f"Found {len(dates)} dates.")
+		if h3item.get("class") == "sigil_not_in_toc" or h3item.has_attr("id") or h3item.has_attr("title"): # only actual letter headers
+			header = h3item.get_text().strip()
+			id = header[:header.find(":")].strip()
+			receiver = header[header.find(":")+1:].strip()
+			time = None
+			place = None # yet undefined
+			letter_body = ""
+			
+			for item in h3item.next_siblings: # verify next p elements
+				if isinstance(item, NavigableString):
+						continue # ignore
+					
+				elif item.has_attr("class"): # <p class="...">
+					item_class = item.get("class")[0]
+					if item_class == "time":
+						time = item.get_text()
 
-    # places
-    places = []
-    for item in soup.find_all("p", {"class": "place"}):
-        previous = str(item.previous_sibling.previous_sibling.encode("utf-8"))
-        if "time" in previous or "card" in previous: # only get class="place" if it comes after class="time" or class="card"
-            places.append(item.get_text())
-    
-    print(f"Found {len(places)} places.")
+					elif item_class == "place":
+						place = item.get_text()
 
-    # letter bodies -- everything in between class="place" and class="link" except classes in to_ignore
-    contents = []
-    letter_body = ""
-    to_ignore = ["date", "card", "place", "time", "sigil_not_in_toc", "imgbrd", "margl"]
-    delimiter = "link"
+					elif item_class in to_ignore:
+						continue
 
-    for tag in soup.find("p", {"class": "place"}).next_siblings:
-        if (not isinstance(tag, NavigableString)) and tag.get("class") is not None:
-            if tag.get("class")[0] in to_ignore:
-                continue
+					elif item_class == delimiter: # end of current letter
+						break # out of <p> loop
+					
+					else: # parts of letter body (<p class="name"> etc.)
+						letter_body += str(item.get_text())
+				
+				else: # <p>
+					letter_body += str(item.get_text())
 
-            if tag.get("class")[0] == delimiter:
-                if len(letter_body.strip().lstrip()) > 0: # skip empty bodies - probably scraping mistakes
-                    contents.append(letter_body)
-                letter_body = ""
-            else:
-                letter_body += str(tag)
-        else:
-            letter_body += str(tag)
-    
-    print(f"Found {len(contents)} letter bodies.")
+			# clean and assemble letter info
+			html_tag_regex = re.compile(r'<[^>]+>')
+			letter_body = html_tag_regex.sub("", letter_body) # remove html tags if any
+			letter_body = letter_body.strip() # remove trailing whitespace
 
+			if len(letter_body.strip()) < 0:
+				print("ERROR: empty letter body :(")
+				exit()
+			
+			# add new line to dataframe
+			vw = vw.append({"id": id, "date": time, "receiver": receiver, "place": place, "text": letter_body}, ignore_index=True)
+			count_per_file += 1
+			
 
-    # remove html tags
-    html_tag_regex = re.compile(r'<[^>]+>')
-    for i in range(len(contents)):
-        contents[i] = contents[i].lstrip().strip() # remove trailing whitespace
-        contents[i] = html_tag_regex.sub("", contents[i]) # remove all html tags
+	# export letter body (for validation)
+	out_all_files = f"{export_path}\\letters.txt"
+	file = codecs.open(out_all_files, mode="a+", encoding="utf-8")
+	file.write(letter_body)
+	file.write("\n--------------\n")
+	file.close()
 
-    # export all letters (for validation)
-    out_all_files = f"{export_path}\\letters.txt"
-    out_current_file = f"{export_path}\\{letters_file.replace('.htm', '.txt')}"
-
-    for letter in contents:
-        file = codecs.open(out_current_file, mode="a+", encoding="utf-8")
-        file.write(letter)
-        file.write("\n--------------\n")
-        file.close()
-
-    # add to complete dataframe    
-    vw_part = pd.DataFrame({"id": ids, "date": dates, "receiver": receivers, "place": places, "text": contents})
-    vw = vw.append(vw_part)
-    print(f"finished reading '{letters_file}'.")
+	print(f"Finished reading '{letters_file}'.")
+	print(f"Added {count_per_file} letters to dataframe.")
+	count_per_file = 0
 
 print("DATAFRAME HEAD:")
 print(vw.head(40).to_string())
