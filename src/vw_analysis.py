4444650f0e5ad2c2af1a9320706bc4dc06c1db80
juliafalcao <juliafsfalcao@hotmail.com>
2019-05-07 23:31:44 -0300

    enfrentando dificuldades pra desvendar o conceito & uso do TF-IDF

--- src/vw_analysis.py
+++ src/vw_analysis.py
@@ -3,16 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import nltk
 from wordcloud import WordCloud
+from sklearn.feature_extraction.text import TfidfVectorizer
+import json
+from scipy.sparse import csr_matrix
 
 pd.set_option('precision', 0)
 
 """
 from basic preprocessed text
 """
-
-vw = pd.read_csv("data\\vw\\vw_preprocessed.csv", index_col="index")
-print("DATAFRAME SAMPLE:")
-print(vw.sample(10).to_string())
+vw = pd.read_csv("data/vw/vw_preprocessed.csv", index_col="index")
+# print("DATAFRAME SAMPLE:")
+# print(vw.sample(10).to_string())
 
 # amount of letters per year
 years = list(vw[vw["year"].notnull()]["year"])
@@ -20,27 +22,25 @@ plt.hist(years, bins = len(set(years)), rwidth=0.8, color="lightseagreen", alpha
 plt.title("Amount of letters written by Virginia Woolf")
 plt.xlabel("Years")
 plt.ylabel("Letters")
-plt.savefig("graphs\\letters_per_year.png")
-plt.cla()
+# plt.savefig("graphs/letters_per_year.png")
 plt.clf()
 
 years = vw[vw["year"].notnull()][["year"]]
 years = years.groupby(["year"]).size()
-print("\nAMOUNT OF LETTERS SENT PER YEAR:")
-print(years.to_string())
+# print("\nAMOUNT OF LETTERS SENT PER YEAR:")
+# print(years.to_string())
 
 # amount of letters per recipient
 recs = vw[vw["recipient"].notnull()][["recipient"]]
 recs = recs.groupby(["recipient"]).size()
 recs = recs.sort_values(ascending=False)
-print("\nTOP RECIPIENTS (20+ letters):")
-print(recs[recs >= 20].to_string())
+# print("\nTOP RECIPIENTS (20+ letters):")
+# print(recs[recs >= 20].to_string())
 
 # average length
 avg_length = np.average(vw["length"])
-print(f"\nAVERAGE LENGTH OF LETTERS: {int(avg_length)} characters")
-
-print(vw.sort_values(by="length", ascending=False))
+# print(f"\nAVERAGE LENGTH OF LETTERS: {int(avg_length)} characters")
+# print(vw.sort_values(by="length", ascending=False))
 
 
 """
@@ -54,7 +54,7 @@ def concat_all_letters(letters_series): # pass series of letters as lists of tok
 	return all_letters
 
 
-vw = pd.read_json("data\\vw\\vw_tokenized.json")
+vw = pd.read_json("data/vw/vw_tokenized.json")
 # print(vw.sample(20).to_string())
 
 # word frequency
@@ -63,10 +63,11 @@ all_words = [word for letter in letters for word in letter]
 word_freq = nltk.FreqDist(all_words)
 plt.ion()
 word_freq.plot(40, cumulative=False, title="Most frequent words (after preprocessing)")
-plt.savefig("graphs\\word_frequency_hist.png")
+plt.savefig("graphs/word_frequency_hist.png")
 plt.ioff()
 
 # word clouds per year
+"""
 years = sorted(set(vw[vw["year"].notnull()]["year"]))
 
 for year in years:
@@ -74,5 +75,40 @@ for year in years:
 	wordcloud = WordCloud(width=500, height=400, background_color="white", max_words=100).generate(letters)
 	plt.imshow(wordcloud, interpolation="bilinear")
 	plt.axis("off")
-	plt.savefig(f"graphs\\word_clouds\\wordcloud_{int(year)}.png")
-	
+	plt.savefig(f"graphs/word_clouds/wordcloud_{int(year)}.png")
+"""
+plt.clf()
+
+"""
+TF-IDF experiments
+too many stop words, virginia
+"""
+def dummy_function(text):
+	return text # so that TfidfVectorizer won't tokenize and preprocess my already tokenized and preprocessed text
+
+yvw = vw[["year"]].drop_duplicates(keep="first").sort_values(by="year")
+yvw = yvw[yvw["year"].notnull()]
+yvw["letters"] = vw["year"].apply(lambda year: concat_all_letters(vw[vw["year"] == year]["text"]))
+yvw = yvw.reset_index()[["year", "letters"]]
+yvw.to_csv("data/vw/yearly_vw.csv")
+
+# tfidf_df = pd.DataFrame(result.todense()) # lines: documents, columns: words, values: tfidf scores
+# tfidf_df = tfidf_df.rename(columns=vocab)
+# print(tfidf_df[[col for col in tfidf_df.columns if col not in tfidf_df.isnull().any()]])
+
+wordcloud = WordCloud(width=500, height=400, background_color="white", max_words=100)
+
+tfidf = TfidfVectorizer(analyzer="word", preprocessor=dummy_function)
+result = tfidf.fit_transform(yvw["letters"])
+vocab = {int(key):term for (term, key) in tfidf.vocabulary_.items()}
+with open("graphs/tfidf/vocab.json", "w+") as vocab_out:
+	json.dump(vocab, vocab_out)
+
+for y in range(len(yvw)):
+	frequencies = {vocab[term]:result[(y, term)] for term in result[y].indices}
+	wordcloud.generate_from_frequencies(frequencies)
+	plt.imshow(wordcloud, interpolation="bilinear")
+	plt.axis("off")
+	year = yvw.at[y, "year"]
+	plt.savefig(f"graphs/tfidf/tfidf_wordcloud_{year}.png")
+	plt.clf()
